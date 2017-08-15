89c86a2d7cb5aabeca34c047dc0fcbd80f02a0af
Nicolas Coburn <Nicolas_Coburn@Nicolas.local>
2017-08-14 22:41:15 -0400

    Finished?

--- js/main.js
+++ js/main.js
@@ -16,252 +16,77 @@ var database = firebase.database();
 
 var reservationData = {};
 
+getReservations();
+
 $('#reserve').on('submit', function(e) {
-	e.preventDefault();
+  // prevent the page from reloading
+  e.preventDefault();
+  // check form is filled out
+  $('.error').text('');
+  if ($('#reservationName').val() == '' || $('#reservationDate').val() == '') {
+    $('.error').text('Please fill out entire form.');
+  } else {
+  // grab name from input field
 	var reservationName = $('#reservationName').val();
-	reservationData.name = reservationName;
-})
+  var reservationDate = $('#reservationDate').val();
+  // clear name from the input (for UX purposes)
+	$('#reservationName').val('');
+  $('#reservationDate').val('');
+  // create a section for comments data in your db
+  var reservationData = database.ref('reservations');
+  // use the set method to save data to the comments
+  reservationData.push({
+    name: reservationName,
+    date: reservationDate
+  });
+  getReservations();
+};
+});
+
+// Handlebars
+
+function getReservations() {
+  database.ref('reservations').on('value', function (results) {
+    var allReservations = results.val();
+    var reservations = [];
+    for (var item in allReservations) {
+      var context = {
+        name: allReservations[item].name,
+        date: allReservations[item].date,
+      };
+      var source = $("#reservation-template").html();
+      var template = Handlebars.compile(source);
+      var reservationListElement = template(context);
+      reservations.push(reservationListElement);
+    }
+    // remove all list items from DOM before appending list items
+    $('.reservations-list').empty()
+    // append each comment to the list of comments in the DOM
+    for (var i in reservations) {
+      $('.reservations-list').append(reservations[i])
+    }
+  });
+};
 
 // Google Map API
 function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.8054491, lng: -73.9654415},
-    zoom: 12,
+    zoom: 10,
+    scrollwheel: false,
     styles: [
-  {
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#ebe3cd"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#080d43"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#f5f1e6"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#c9b2a6"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#dcd2be"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#080d43"
-      }
-    ]
-  },
-  {
-    "featureType": "landscape.natural",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#dfd2ae"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#dfd2ae"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#080d43"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.business",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#ebecf6"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#080d43"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#f5f1e6"
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#fdfcf8"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#f8c967"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#e9bc62"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway.controlled_access",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#e98d58"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway.controlled_access",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#db8555"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#080d43"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#dfd2ae"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#080d43"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#080d43"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.station",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#dfd2ae"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#FFFFFF"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#080d43"
-      }
-    ]
-  }
+    {
+      featureType: "all",
+      elementType: "all",
+      stylers: [
+        { saturation: -100 } // <-- THIS
+      ]
+    }
 ]
     });
 	var marker = new google.maps.Marker({
 		position: {lat: 40.8054491, lng: -73.9654415},
-		map: map
+		map: map,
+    title: 'Monks Café'
 	});
 };
